597018225789226a6eef3ec3de5ea410b9ab9214
Ayoub SAHI <ayoubsahi@outlook.com>
2023-01-13 00:46:29 +0100

    implemented radius and border extrapolation in the gui

--- ImageFilters.GUI/Helpers/Converters.cs
+++ ImageFilters.GUI/Helpers/Converters.cs
@@ -1,22 +1,45 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ImageFilters.GUI.Helpers;
 
 [ValueConversion(typeof(bool), typeof(bool))]
-internal class BoolInverter: IValueConverter
+internal class BoolInverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        bool booleanValue = (bool)value;
-        return !booleanValue;
-    }
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		bool booleanValue = (bool)value;
+		return !booleanValue;
+	}
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        bool booleanValue = (bool)value;
-        return !booleanValue;
-    }
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		bool booleanValue = (bool)value;
+		return !booleanValue;
+	}
+}
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+internal class BoolToVisibilityInversed : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		bool booleanValue = (bool)value;
+
+		return (bool)value ? Visibility.Hidden : Visibility.Visible;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var visibility = (Visibility)value;
+
+		return visibility switch
+		{
+			Visibility.Visible => false,
+			_ => true
+		};
+	}
 }
--- ImageFilters.GUI/Helpers/Extensions.cs
+++ ImageFilters.GUI/Helpers/Extensions.cs
@@ -1,42 +1,86 @@
 ﻿using System.Drawing;
 using System.IO;
+using System.Windows.Markup;
 using System.Windows.Media.Imaging;
 
 namespace ImageFilters.GUI.Helpers;
 
 public static class Extensions
 {
-    public static Bitmap ToGdiImage(this BitmapSource bitmapImage)
-    {
-        using MemoryStream outStream = new();
-        var enc = new BmpBitmapEncoder();
-        enc.Frames.Add(BitmapFrame.Create(bitmapImage));
-        enc.Save(outStream);
+	public static Bitmap ToGdiImage(this BitmapSource bitmapImage)
+	{
+		using MemoryStream outStream = new();
+		var enc = new BmpBitmapEncoder();
+		enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+		enc.Save(outStream);
 
-        return new Bitmap(outStream);
-    }
+		return new Bitmap(outStream);
+	}
 
-    public static BitmapImage ToBitmapImage(this Bitmap bitmap)
-    {
-        using MemoryStream memory = new();
-        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
-        memory.Position = 0;
-        BitmapImage bitmapimage = new();
-        bitmapimage.BeginInit();
-        bitmapimage.StreamSource = memory;
-        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-        bitmapimage.EndInit();
+	public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+	{
+		using MemoryStream memory = new();
+		bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+		memory.Position = 0;
+		BitmapImage bitmapimage = new();
+		bitmapimage.BeginInit();
+		bitmapimage.StreamSource = memory;
+		bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+		bitmapimage.EndInit();
 
-        return bitmapimage;
-    }
+		return bitmapimage;
+	}
 
-    public static void Save(this BitmapImage image, string filePath)
-    {
-        BitmapEncoder encoder = new PngBitmapEncoder();
-        encoder.Frames.Add(BitmapFrame.Create(image));
+	public static void Save(this BitmapImage image, string filePath)
+	{
+		BitmapEncoder encoder = new PngBitmapEncoder();
+		encoder.Frames.Add(BitmapFrame.Create(image));
 
-        using var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create);
-        encoder.Save(fileStream);
-    }
+		using var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create);
+		encoder.Save(fileStream);
+	}
+}
 
+public class EnumBindingSourceExtension : MarkupExtension
+{
+	private Type? _enumType;
+	public Type? EnumType
+	{
+		get => _enumType;
+		set
+		{
+			if (value == _enumType) return;
+			if (value is not null)
+			{
+				var enumType = Nullable.GetUnderlyingType(value) ?? value;
+				if (!enumType.IsEnum)
+					throw new ArgumentException("Type must be for an Enum.");
+			}
+
+			_enumType = value;
+		}
+	}
+
+	public EnumBindingSourceExtension() { }
+
+	public EnumBindingSourceExtension(Type? enumType)
+	{
+		this.EnumType = enumType;
+	}
+
+	public override object ProvideValue(IServiceProvider serviceProvider)
+	{
+		if (null == this._enumType)
+			throw new InvalidOperationException("The EnumType must be specified.");
+
+		Type actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
+		Array enumValues = Enum.GetValues(actualEnumType);
+
+		if (actualEnumType == this._enumType)
+			return enumValues;
+
+		Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+		enumValues.CopyTo(tempArray, 1);
+		return tempArray;
+	}
 }
