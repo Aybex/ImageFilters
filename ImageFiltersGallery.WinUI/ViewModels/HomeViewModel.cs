4feb687654c14de1574b2395f0b1a8c4afd9068b
Ayoub SAHI <ayoubsahi@outlook.com>
2023-10-25 23:57:39 +0100

    making the ui like windows settings

---
+++ ImageFiltersGallery.WinUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ImageFiltersGallery.WinUI.ViewModels;
+
+public partial class HomeViewModel: ObservableObject
+{
+    [ObservableProperty] private string name = "Home Page Name";
+    [ObservableProperty] private bool buttonEnabled = true;
+
+}
